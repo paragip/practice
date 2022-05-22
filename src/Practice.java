a2139a6b83df8fda14ef3b0d7e5622661ef11aa5
paragip <pal.paragi@gmail.com>
2022-05-22 15:42:44 +0200

    int min max

---
+++ src/Practice.java
@@ -0,0 +1,7 @@
+public class Practice {
+    public static void main(String[] args) {
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println(minValue + " " + maxValue);
+    }
+}
