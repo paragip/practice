59eed5ed0725ad308949aef73a9e4da9a4a573f1
paragip <pal.paragi@gmail.com>
2022-05-28 21:12:52 +0200

    challenge

--- src/Practice.java
+++ src/Practice.java
@@ -1,7 +1,13 @@
 public class Practice {
     public static void main(String[] args) {
-        int age = 3;
-        boolean isAdult = (age > 18) ? true : false;
-        System.out.println("this is " + isAdult);
+        double numberOne = 20.00;
+        double numberTwo = 80.00;
+        double numberThree = (numberOne + numberTwo) * 100;
+        double remainder = numberThree % 40.00;
+        boolean isRemainderOrNot = (remainder == 0) ? true : false;
+        System.out.println(isRemainderOrNot);
+        if (!isRemainderOrNot) {
+            System.out.println("Got some remainder");
+        }
     }
 }
