6f3a1f20ad689799314667046b9ec0dbe34ec672
paragip <pal.paragi@gmail.com>
2022-05-28 22:50:21 +0200

    method minta

--- src/Practice.java
+++ src/Practice.java
@@ -1,13 +1,18 @@
 public class Practice {
     public static void main(String[] args) {
-        double numberOne = 20.00;
-        double numberTwo = 80.00;
-        double numberThree = (numberOne + numberTwo) * 100;
-        double remainder = numberThree % 40.00;
-        boolean isRemainderOrNot = (remainder == 0) ? true : false;
-        System.out.println(isRemainderOrNot);
-        if (!isRemainderOrNot) {
-            System.out.println("Got some remainder");
+
+        calculateScore(false, 10000, 8, 200);
+
+        calculateScore(true, 15000, 10, 200);
+
+
+    }
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+        if (gameOver) {
+            int finalScore = score + levelCompleted * bonus;
+            System.out.println("final score = " + finalScore);
+        } else {
+            System.out.println("the game has not ended");
         }
     }
 }
