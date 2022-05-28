9a91e87ae16e41b02f411376dd836f117a96ca85
paragip <pal.paragi@gmail.com>
2022-05-28 20:58:50 +0200

    age test

--- src/Practice.java
+++ src/Practice.java
@@ -1,9 +1,7 @@
 public class Practice {
     public static void main(String[] args) {
-        double poundValue = 5.0;
-        double onePoundEqualKg = 0.45359237;
-        double resultKg;
-        resultKg = onePoundEqualKg * poundValue;
-        System.out.println(resultKg);
+        int age = 3;
+        boolean isAdult = (age > 18) ? true : false;
+        System.out.println("this is " + isAdult);
     }
 }
